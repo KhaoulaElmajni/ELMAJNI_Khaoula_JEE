c8c5a6e6546964c432c939e738c38bd83f7831f3
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-03-05 12:54:42 +0100

    modification de l'activité 3

--- Activité_Pratique_semaine3/Jpa-activite-pratique/src/test/java/ma/enset/jpaactivitepratique/JpaActivitePratiqueApplicationTests.java
+++ Activité_Pratique_semaine3/Jpa-activite-pratique/src/test/java/ma/enset/jpaactivitepratique/JpaActivitePratiqueApplicationTests.java
@@ -1,10 +1,10 @@
-package ma.enset.jpaap;
+package ma.enset.jpaactivitepratique;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JpaApApplicationTests {
+class JpaActivitePratiqueApplicationTests {
 
     @Test
     void contextLoads() {
