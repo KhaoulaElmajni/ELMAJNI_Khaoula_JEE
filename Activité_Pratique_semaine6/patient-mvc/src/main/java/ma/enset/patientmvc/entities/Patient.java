2fe40f6e2f561380dd20c1567b340443b1f6c608
khaoula elmajni <elmajnikhaoula99@gmail.com>
2022-03-26 17:22:15 +0100

    l'ajout d'un patient avec la validation des shamps

--- Activité_Pratique_semaine6/patient-mvc/src/main/java/ma/enset/patientmvc/entities/Patient.java
+++ Activité_Pratique_semaine6/patient-mvc/src/main/java/ma/enset/patientmvc/entities/Patient.java
@@ -1,12 +1,17 @@
 package ma.enset.patientmvc.entities;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
-
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data @NoArgsConstructor
@@ -14,9 +19,15 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min = 5,max = 30)
     private String nom;
-    @Temporal(TemporalType.DATE)
+    //@Temporal(TemporalType.DATE)
+    @NotNull
+    @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
     private Date dateNaissance;
     private boolean malade;
+    @NotNull
+    @DecimalMin("100")
     private int score;
 }
